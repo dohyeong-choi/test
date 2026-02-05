d3853a2c2acb2b41885b671eb180720d339bc4c9
dohyeong-choi <dc65@illinois.edu>
2026-02-05 15:17:05 -0600

    Update test.jl

--- test.jl
+++ test.jl
@@ -8,4 +8,16 @@ println("Is 1 + ϵ/2 > 1? $(1.0 + eps(Float64)/2 > 1.0)")
 println("The smallest representable number larger than 1.0 is $(nextfloat(1.0))")
 println("The largest representable number smaller than 1.0 is $(prevfloat(1.0))")
 
+x = 1.2 - 1.1
+x == 0.1
 
+
+deriv_x_squred = function (x, h)
+
+    return (((x+h)^2 - x^2) / h)
+end
+
+
+deriv_x_squred(2,1e-1)
+deriv_x_squred(2,1e-12)
+deriv_x_squred(2,1e-30)
